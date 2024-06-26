04d98553c6ec0bbebc60ec1208292cca5192d736
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2024-06-26 17:56:27 +1000

    Add validation tests
    - check path length
    - add more config validation tests

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -1,6 +1,33 @@
 test_that("config is validated", {
-    bad <- new_config(
-        path = "."
+    bad <- new_config(path = ".")
+    bad2 <- new_config(
+        path = c(
+            system.file("examples", "plumber.R", package = "hotwater"),
+            system.file("examples", "plumber.R", package = "hotwater")
+        )
     )
-    expect_error(validate_config(bad))
+    bad3 <- new_config(
+        path = system.file("examples", "plumber.R", package = "hotwater"),
+        dirs = system.file("examples", "plumber.R", package = "hotwater")
+    )
+    bad4 <- new_config(
+        path = system.file("examples", "plumber.R", package = "hotwater"),
+        port = "not a port"
+    )
+    expect_error(
+        validate_config(bad),
+        class = new_hotwater_error("invalid_path")
+    )
+    expect_error(
+        validate_config(bad2),
+        class = new_hotwater_error("invalid_path_length")
+    )
+    expect_error(
+        validate_config(bad3),
+        class = new_hotwater_error("invalid_dir")
+    )
+    expect_error(
+        validate_config(bad4),
+        class = new_hotwater_error("invalid_port")
+    )
 })
