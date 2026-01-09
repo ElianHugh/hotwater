3d05361f99e2953b4c1f99ba1a8e9a2c8ac9a5e7
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2026-01-09 11:33:31 +1100

    Update plumber.R

--- inst/examples/plumber.R
+++ inst/examples/plumber.R
@@ -1,14 +1,5 @@
 #' @get /
 #' @serializer html
 function() {
-    print("got here?")
     "Hello, world!"
-    stop("oh no")
 }
-
-print('/foo')
-
-message("foo")
-
-rlang::abort("foo")
-stop('oh no')
